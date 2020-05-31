61ad9350bf6f2586e8fa53cadb6802f2326a956e
yumi.noguchi <anzu1027@gmail.com>
2020-05-31 14:10:23 +0000

    shusei

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -49,7 +49,7 @@ class TasksController < ApplicationController
     end
     
     def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content,:status)
     end
      
 end
