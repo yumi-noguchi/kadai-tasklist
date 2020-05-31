7a24305e5fe73f7fb316cdfc8fd6f67198e4f068
yumi.noguchi <anzu1027@gmail.com>
2020-05-31 08:48:59 +0000

    CRUD messages3

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,7 +4,7 @@ class TasksController < ApplicationController
     end
     
     def show
-        @tasks = Task.find(params[:id])
+        @task = Task.find(params[:id])
     end
     
     def new
